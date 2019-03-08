331bb1baf541574fc12079a64cc99207f8b4924d
lipeng <1019878083@qq.com>
2019-03-08 11:59:25 +0800

    电影讯息列表模块视图层界面结构开发完成，将开发model层准备渲染

--- front-end/src/javascripts/controllers/movie/app-movie-items-controller.js
+++ front-end/src/javascripts/controllers/movie/app-movie-items-controller.js
@@ -1,5 +1,6 @@
 
 import appMovieItems from '@views/routes/app-movie-items.html'
+import {getMovieItems} from '@models/movie-model'
 
 const render = (req, res, next) => {
     res.render(appMovieItems)
