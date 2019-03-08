5d27a1ac833eb675fa7fdc6c427ad76768f7fe28
lipeng <1019878083@qq.com>
2019-03-08 22:21:48 +0800

    发布页控制层

--- front-end/src/javascripts/controllers/movie/app-movie-publish-controller.js
+++ front-end/src/javascripts/controllers/movie/app-movie-publish-controller.js
@@ -3,6 +3,28 @@ import appMoviePublish from '@views/routes/app-movie-publish.html'
 
 const render = (req, res, next) => {
     res.render(appMoviePublish)
+
+    // 初始化日历插件
+    $('#datepicker').date_input()
+    bindEvents() // 绑定各种事件
+}
+
+//事件绑定函数封装
+function bindEvents () {
+    $('#publish-form').submit(async function(e){
+        e.preventDefault()
+        let title = $('#item-title').val()
+        let description = $('#item-description').val()
+        let star = $('#item-star').val()
+        let showTime = $('#datepicker').val()
+        console.log(title, description, star, showTime)
+
+        let data = await postMovieItem({
+            title, description, star, showTime
+        })
+        console.log(data)
+
+    })
 }
 
 export default {
